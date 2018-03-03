05f8d54b14a4f9252a999255a18bf15dccbaf5b1
Joo <joo@joostory.net>
2018-03-04 00:50:20 +0900

    init

---
+++ src/OpengraphFetcher.test.js
@@ -0,0 +1,18 @@
+import fetcher from './OpengraphFetcher'
+
+test('import Opengraph Fetcher', () => {
+  expect(fetcher).not.toBeNull()
+})
+
+test('fetch youtube', done => {
+  fetcher.fetch('https://youtu.be/d6jnImCVanY', opengraph => {
+    expect(opengraph).not.toBeNull()
+    expect(opengraph.title).toBe('[ENG] 농심에서 또 라면이!? 건면새우탕을 먹어봤습니다.')
+    expect(opengraph.url).toBe('https://www.youtube.com/watch?v=d6jnImCVanY')
+    expect(opengraph.type).toBe('video.other')
+    expect(opengraph.image).toBe('https://i.ytimg.com/vi/d6jnImCVanY/maxresdefault.jpg')
+    expect(opengraph.mediaUrl).toBe('https://www.youtube.com/embed/d6jnImCVanY')
+    expect(opengraph.host).toBe('www.youtube.com')
+    done()
+  })
+})
