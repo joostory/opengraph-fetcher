654777f27ac42580eb9b87843e660bc25099d076
Joo <joo@joostory.net>
2019-08-27 14:33:36 +0900

    fix youtube test

--- src/OpengraphFetcher.test.js
+++ src/OpengraphFetcher.test.js
@@ -7,12 +7,22 @@ test('import Opengraph Fetcher', () => {
 test('fetch youtube', done => {
   fetcher.fetch('https://youtu.be/d6jnImCVanY', opengraph => {
     expect(opengraph).not.toBeNull()
-    expect(opengraph.title).toBe('[ENG] 농심에서 또 라면이!? 건면새우탕을 먹어봤습니다.')
     expect(opengraph.url).toBe('https://www.youtube.com/watch?v=d6jnImCVanY')
     expect(opengraph.type).toBe('video.other')
     expect(opengraph.image).toBe('https://i.ytimg.com/vi/d6jnImCVanY/maxresdefault.jpg')
     expect(opengraph.mediaUrl).toBe('https://www.youtube.com/embed/d6jnImCVanY')
     expect(opengraph.host).toBe('www.youtube.com')
+    done()
+  })
+})
+
+test('fetch github', () => {
+  fetcher.fetch('https://github.com/joostory/opengraph-fetcher', opengraph => {
+    expect(opengraph).not.toBeNull()
+    expect(opengraph.title).toBe('joostory/opengraph-fetcher')
+    expect(opengraph.url).toBe('https://github.com/joostory/opengraph-fetcher')
+    expect(opengraph.type).toBe('object')
+    expect(opengraph.host).toBe('github.com')
     done()
   })
 })
