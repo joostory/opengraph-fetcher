8ac793825ed552447da5962a10de680d27d154c2
Joo <joo@joostory.net>
2022-04-23 14:24:59 +0900

    cli로 동작하도록 수정

---
+++ bin/opengraph-fetcher.js
@@ -0,0 +1,13 @@
+const fetcher = require('../src')
+const { argv, exit } = require('process')
+
+if (argv.length < 3) {
+  console.log("Usage: opengraph-fetcher URL")
+  exit(1)
+}
+
+const url = argv[2]
+fetcher.fetch(url)
+  .then(r => console.log(r))
+  .catch(e => console.error(e))
+
